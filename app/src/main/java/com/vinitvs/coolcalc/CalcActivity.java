ebb78deabb3587f37235daaac4361cbe00fdacb8
Vinit Singh <vinitveersingh@gmail.com>
2017-10-08 19:30:11 -0400

    XML completed

---
+++ app/src/main/java/com/vinitvs/coolcalc/CalcActivity.java
@@ -0,0 +1,13 @@
+package com.vinitvs.coolcalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalcActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
